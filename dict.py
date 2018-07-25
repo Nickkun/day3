7064ea29af001300b75257d79c4dcce63ac4fbcd
Nickkun <nigitm@gmail.com>
2018-07-25 17:48:04 +0900

    dictionary

---
+++ dict.py
@@ -0,0 +1,14 @@
+person = {'name':'Nick', 'address':'cheonan', 'email':'jang@ancle.kr'}
+print(person)
+print(person['name'])
+print(person['email'])
+
+person['name'] = 'Jay'
+
+print(person.items())
+for key, value in person.items():
+    print('\nKey \\ "' + key + '"')
+    print('Value \\ "' + value + '"')
+
+# Key \ "name"
+# Value \ "Nick"
