e507381833b611df4cc0f5720ee3623a62cf05d7
Nickkun <nigitm@gmail.com>
2018-07-26 13:20:28 +0900

    day4 함수 기초까지

---
+++ loopcontrol.py
@@ -0,0 +1,19 @@
+message = ''
+
+# while message != 'quit':
+#     message = input("typing you message: ")
+#     print(">" + message)
+
+# while True: 
+#     message = input("typing you message: ")
+#     print(">" + message)
+#     if message == 'quit':
+#         break
+
+while number < 10:
+    number = number+1
+    if (number%2 == 0):
+        continue
+    print(number)
+
+      
