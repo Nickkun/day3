f4f51337624c1fac0cda5e06df1c27d3d576ef35
Nickkun <nigitm@gmail.com>
2018-07-26 14:29:03 +0900

    함수 매개변수와 return

--- function.py
+++ function.py
@@ -1,7 +1,14 @@
 
-def hello(name):
-    print("Hello, " + name)
+def hello(name='Nick', age='10'):
+    print("Hello, " + name )
+    print(age + " years old")
 
-typing = input("What is your name? ")
-hello(typing)
-print(name)
+# typing = input("What is your name? ")
+# hello(typing)
+
+tp1 = input("Your Name: ")
+tp2 = input("How old are you? ")
+# hello(name, age)
+# hello(name)
+# hello()
+hello(age=tp2, name=tp1)
